ffb89790a3d37475857e6879c8818861bb9df8e8
Dario <dario.buffa@gmail.com>
2025-07-21 05:33:28 -0300

    Added navigation from login to main menu

--- src/main/java/com/ourgroup/personalexpensetracker/controller/LoginController.java
+++ src/main/java/com/ourgroup/personalexpensetracker/controller/LoginController.java
@@ -1,4 +1,24 @@
 package com.ourgroup.personalexpensetracker.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.Parent;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class LoginController {
+
+    public void onLoginButtonClick(ActionEvent event) {
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("/view/MainScreenSummary.fxml"));
+            Stage stage = (Stage) ((javafx.scene.Node)event.getSource()).getScene().getWindow();
+            stage.setScene(new Scene(root));
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
