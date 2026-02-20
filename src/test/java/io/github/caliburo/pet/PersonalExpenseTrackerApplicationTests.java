be2a60f8f509c2963fc0783efa5b1e2c3d741714
Caliburno <dario.buffa@gmail.com>
2026-02-20 12:23:52 -0300

    Initial commit - reboot as Spring Boot web app

---
+++ src/test/java/io/github/caliburo/pet/PersonalExpenseTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.caliburo.pet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalExpenseTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
